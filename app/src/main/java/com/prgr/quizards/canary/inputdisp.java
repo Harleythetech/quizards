aed70badb0d1525d938a1b2699d5856496c84528
john dave <jhndv.ntvdd@gmail.com>
2022-10-11 03:21:53 +0800

    [JohnDave][Mix] mix some codes

--- app/src/main/java/com/prgr/quizards/canary/inputdisp.java
+++ app/src/main/java/com/prgr/quizards/canary/inputdisp.java
@@ -1,28 +1,127 @@
 package com.prgr.quizards.canary;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.AppCompatButton;
-import android.content.Intent;
+import android.app.Activity;
+import android.app.*;
+import android.os.*;
+import android.view.*;
+import android.view.View.*;
+import android.widget.*;
+import android.content.*;
+import android.content.res.*;
+import android.graphics.*;
+import android.graphics.drawable.*;
+import android.media.*;
+import android.net.*;
+import android.text.*;
+import android.text.style.*;
+import android.util.*;
+import android.webkit.*;
+import android.animation.*;
+import android.view.animation.*;
+import java.io.*;
+import java.util.*;
+import java.util.regex.*;
+import java.text.*;
+import org.json.*;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.content.SharedPreferences;
+import android.view.View;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.DialogFragment;
 
-import android.os.Bundle;
+public class inputdisp extends Activity {
 
-public class inputdisp extends AppCompatActivity {
+    private EditText quizname;
+    private EditText desc;
+    private EditText amount;
+    private Button button;
+
+
+    private SharedPreferences jshared;
+    private Intent intent = new Intent();
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle _savedInstanceState) {
+        super.onCreate(_savedInstanceState);
         setContentView(R.layout.activity_inputdisp);
-        AppCompatButton btn = findViewById(R.id.button3);
-        btn.setOnClickListener(v -> gotoback());
-        AppCompatButton btn2 = findViewById(R.id.button);
-        btn2.setOnClickListener(v -> gotowrite());
+        initialize(_savedInstanceState);
+        initializeLogic();
     }
-    public void gotowrite(){
-        Intent intent = new Intent(this, question.class);
-        startActivity(intent);
+
+    private void initialize(Bundle _savedInstanceState) {
+        quizname = findViewById(R.id.quizname);
+        desc = findViewById(R.id.desc);
+        amount = findViewById(R.id.amount);
+        button = findViewById(R.id.button);
+        jshared = getSharedPreferences("j", Activity.MODE_PRIVATE);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View _view) {
+                jshared.edit().putString("quizname", quizname.getText().toString()).commit();
+                jshared.edit().putString("desc", desc.getText().toString()).commit();
+                jshared.edit().putString("amount", amount.getText().toString()).commit();
+                getIntent().setClass(getApplicationContext(), question.class);
+                        startActivity(intent);
+            }
+        });
     }
-    public void gotoback(){
-        Intent intent = new Intent(this, activity_home_screen.class);
-        startActivity(intent);
+
+    private void initializeLogic() {
+    }
+
+
+    @Deprecated
+    public void showMessage(String _s) {
+        Toast.makeText(getApplicationContext(), _s, Toast.LENGTH_SHORT).show();
+    }
+
+    @Deprecated
+    public int getLocationX(View _v) {
+        int _location[] = new int[2];
+        _v.getLocationInWindow(_location);
+        return _location[0];
+    }
+
+    @Deprecated
+    public int getLocationY(View _v) {
+        int _location[] = new int[2];
+        _v.getLocationInWindow(_location);
+        return _location[1];
+    }
+
+    @Deprecated
+    public int getRandom(int _min, int _max) {
+        Random random = new Random();
+        return random.nextInt(_max - _min + 1) + _min;
+    }
+
+    @Deprecated
+    public ArrayList<Double> getCheckedItemPositionsToArray(ListView _list) {
+        ArrayList<Double> _result = new ArrayList<Double>();
+        SparseBooleanArray _arr = _list.getCheckedItemPositions();
+        for (int _iIdx = 0; _iIdx < _arr.size(); _iIdx++) {
+            if (_arr.valueAt(_iIdx))
+                _result.add((double)_arr.keyAt(_iIdx));
+        }
+        return _result;
+    }
+
+    @Deprecated
+    public float getDip(int _input) {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, _input, getResources().getDisplayMetrics());
+    }
+
+    @Deprecated
+    public int getDisplayWidthPixels() {
+        return getResources().getDisplayMetrics().widthPixels;
+    }
+
+    @Deprecated
+    public int getDisplayHeightPixels() {
+        return getResources().getDisplayMetrics().heightPixels;
     }
 }
--- app/src/main/java/com/prgr/quizards/canary/question.java
+++ app/src/main/java/com/prgr/quizards/canary/question.java
@@ -3,26 +3,114 @@ package com.prgr.quizards.canary;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 import android.content.Intent;
+import android.app.Activity;
+import android.app.*;
+import android.os.*;
+import android.view.*;
+import android.view.View.*;
+import android.widget.*;
+import android.content.*;
+import android.content.res.*;
+import android.graphics.*;
+import android.graphics.drawable.*;
+import android.media.*;
+import android.net.*;
+import android.text.*;
+import android.text.style.*;
+import android.util.*;
+import android.webkit.*;
+import android.animation.*;
+import android.view.animation.*;
+import java.io.*;
+import java.util.*;
+import java.util.regex.*;
+import java.text.*;
+import org.json.*;
+import android.widget.LinearLayout;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.TextView;
+import android.content.SharedPreferences;
+import android.view.View;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.DialogFragment;
 
-import android.os.Bundle;
+public class question extends Activity {
 
-public class question extends AppCompatActivity {
+
+    private TextView text;
+
+    private SharedPreferences jshared;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle _savedInstanceState) {
+        super.onCreate(_savedInstanceState);
         setContentView(R.layout.activity_question);
-        AppCompatButton btn = findViewById(R.id.button3);
-        btn.setOnClickListener(v -> gotoback());
-        AppCompatButton btn2 = findViewById(R.id.button);
-        btn2.setOnClickListener(v -> gotowrite());
+        initialize(_savedInstanceState);
+        initializeLogic();
     }
-    public void gotowrite(){
-        Intent intent = new Intent(this, answerscrn.class);
-        startActivity(intent);
+
+    private void initialize(Bundle _savedInstanceState) {
+
+        text = findViewById(R.id.text);
+        jshared = getSharedPreferences("jshared", Activity.MODE_PRIVATE);
+
     }
-    public void gotoback(){
-        Intent intent = new Intent(this, inputdisp.class);
-        startActivity(intent);
+
+    private void initializeLogic() {
+        text.setText(jshared.getString("amount", ""));
+    }
+
+
+    @Deprecated
+    public void showMessage(String _s) {
+        Toast.makeText(getApplicationContext(), _s, Toast.LENGTH_SHORT).show();
+    }
+
+    @Deprecated
+    public int getLocationX(View _v) {
+        int _location[] = new int[2];
+        _v.getLocationInWindow(_location);
+        return _location[0];
+    }
+
+    @Deprecated
+    public int getLocationY(View _v) {
+        int _location[] = new int[2];
+        _v.getLocationInWindow(_location);
+        return _location[1];
+    }
+
+    @Deprecated
+    public int getRandom(int _min, int _max) {
+        Random random = new Random();
+        return random.nextInt(_max - _min + 1) + _min;
+    }
+
+    @Deprecated
+    public ArrayList<Double> getCheckedItemPositionsToArray(ListView _list) {
+        ArrayList<Double> _result = new ArrayList<Double>();
+        SparseBooleanArray _arr = _list.getCheckedItemPositions();
+        for (int _iIdx = 0; _iIdx < _arr.size(); _iIdx++) {
+            if (_arr.valueAt(_iIdx))
+                _result.add((double)_arr.keyAt(_iIdx));
+        }
+        return _result;
+    }
+
+    @Deprecated
+    public float getDip(int _input) {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, _input, getResources().getDisplayMetrics());
+    }
+
+    @Deprecated
+    public int getDisplayWidthPixels() {
+        return getResources().getDisplayMetrics().widthPixels;
+    }
+
+    @Deprecated
+    public int getDisplayHeightPixels() {
+        return getResources().getDisplayMetrics().heightPixels;
     }
 }
